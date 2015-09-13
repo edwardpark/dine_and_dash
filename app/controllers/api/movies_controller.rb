501668f5afa7d131eec40cf0ef62e7c9ce1e7ce4
Edward Park <parkes.edward@gmail.com>
2015-09-13 16:45:58 -0400

    added http party gem and /movies endpoint

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,7 +1,7 @@
 module API
   class MoviesController < ApplicationController
     def index
-      movies = Movie.all
+      movies = HTTParty.get("http://data.tmsapi.com/v1.1/movies/showings?startDate=2015-09-13&zip=22066&imageSize=Sm&api_key="+ENV["gracenote_api_key"])
       render json: movies, status: 200
     end
     #might want to checkout activemodel serielizers for more scalable solution
