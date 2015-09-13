c7b43c6fb84818c479e1c30cc70246ac991d22d0
Edward Park <parkes.edward@gmail.com>
2015-09-13 13:45:53 -0400

    namespaced api routes and added moviesController

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,11 @@
 Rails.application.routes.draw do
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
    root 'welcome#index'
 
-   resources :movies, only: [ :index, :show ], constraints:{subdomain: 'api'}
+   constraints subdomain: 'api' do #note subdomain api might cause trouble in deployment
+     namespace :api do
+       resources :movies, only: [:index,:show]
+     end
+   end
 
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
 end
