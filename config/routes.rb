47a331c82267258de14c9fbf35f5092faae73c61
Edward Park <parkes.edward@gmail.com>
2015-09-13 13:49:53 -0400

    added path: to namespace to reduce uri repitition

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
    root 'welcome#index'
 
    constraints subdomain: 'api' do #note subdomain api might cause trouble in deployment
-     namespace :api do
+     namespace :api, path: '/' do
        resources :movies, only: [:index,:show]
      end
    end
