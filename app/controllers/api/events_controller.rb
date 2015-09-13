c9532b72b73fb393d8add88932df35a3723435b5
Edward Park <parkes.edward@gmail.com>
2015-09-13 19:03:02 -0400

    replaced event json return with empty header return to even create endpoint

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -4,7 +4,10 @@ module API
       def create
         event = Event.new(event_params)
         if event.save
-          render json: event, status: 201 #location: event  would set location  header to url event/:id
+          render nothing: true, status: 204 #location: event  would set location  header to url event/:id
+          #returning nothing: true returns a empty response body
+        else
+          render json: event.errors, status: 422
         end
       end
 
