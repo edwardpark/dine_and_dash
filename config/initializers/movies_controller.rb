122ba7d8d417846b58944a3142c681a57c26d94e
Edward Park <parkes.edward@gmail.com>
2015-09-14 09:45:42 -0400

    removed API subdomain and namespacing

--- config/initializers/movies_controller.rb
+++
@@ -1,4 +0,0 @@
-ActiveSupport::Inflector.inflections(:en) do |inflect|
-  inflect.acronym 'API'
-end
-# This will Allow us to use 'api' in all caps
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,9 @@ Rails.application.routes.draw do
 
    root 'welcome#index'
 
-   constraints subdomain: 'api' do #note subdomain api might cause trouble in deployment
-     namespace :api, path: '/' do
-       resources :movies, only: [:index,:show]
-       resources :events, only:[:create]
-     end
-   end
+
+     resources :movies, only: [:index,:show]
+     resources :events, only:[:create]
+
 
 end
